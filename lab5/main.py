7298aea9d9916c31131b986b31ecc06ac8441ef9
Daniil Kirillov <chunaixxx@gmail.com>
2020-11-05 19:04:34 +0300

    Add main.py in lab5

---
+++ lab5/main.py
@@ -0,0 +1,129 @@
+from random import (randint)
+
+# Количество строк и стобцов в двумерном массиве
+rowLength = 10
+colLength = 10
+
+# Возвращает двумерный массив Row x Col со случайными значениями
+# В диапазоне от min до max
+def makeRandomArr(row, col, min, max):
+	arr = [[0] * col for i in range(row)]
+
+	for Row in range(row):
+		for Col in range(col):
+			arr[Row][Col] = randint(min, max)
+
+	return arr
+
+# Выводит количество локальных минимумов
+def findLocalMin(arr):
+	count = 0
+
+	for Row in range(rowLength):
+		for Col in range(colLength):
+			# Текущий элемент который нужно проверить
+			el = arr[Row][Col]
+
+			# Проверка первой строки
+			if (Row == 0):
+				if (Col == 0):
+					# Проверка элемента в левом верехнем углу
+					if  (el < arr[Row][Col + 1])\
+					and (el < arr[Row + 1][Col + 1])\
+					and (el < arr[Row + 1][Col]):
+						count += 1
+				elif (Col == colLength - 1):
+					# Проверка элемента в правом верхнем углу
+					if  (el < arr[Row][Col - 1])\
+					and (el < arr[Row + 1][Col - 1])\
+					and (el < arr[Row + 1][Col]):
+						count += 1
+				else:
+					# Проверка остальных элементов в первой строке
+					if  (el < arr[Row][Col - 1])\
+					and (el < arr[Row + 1][Col - 1])\
+					and (el < arr[Row + 1][Col])\
+					and (el < arr[Row + 1][Col + 1])\
+					and (el < arr[Row][Col + 1]):
+						count += 1
+
+			# Проверка последней строки
+			if (Row == rowLength - 1):
+				if (Col == 0):
+					# Проверка элемента в левом нижнем углу
+					if  (el < arr[Row - 1][Col])\
+					and (el < arr[Row - 1][Col + 1])\
+					and (el < arr[Row][Col + 1]):
+						count += 1
+				elif (Col == colLength - 1):
+					# Проверка элемента в правом верхнем углу
+					if  (el < arr[Row - 1][Col])\
+					and (el < arr[Row - 1][Col - 1])\
+					and (el < arr[Row][Col - 1]):
+						count += 1
+				else:
+					# Проверка остальных элементов в поселедней строке
+					if  (el < arr[Row][Col - 1])\
+					and (el < arr[Row - 1][Col - 1])\
+					and (el < arr[Row - 1][Col])\
+					and (el < arr[Row - 1][Col + 1])\
+					and (el < arr[Row][Col + 1]):
+						count += 1
+
+			# Проверка остальных строк (промежуточных)
+			if (Row != rowLength - 1) and (Row != 0):
+				# Проверка левого крайнего элемента
+				if (Col == 0):
+					if  (el < arr[Row - 1][Col])\
+					and (el < arr[Row + 1][Col])\
+					and (el < arr[Row][Col + 1])\
+					and (el < arr[Row + 1][Col + 1])\
+					and (el < arr[Row - 1][Col + 1]):
+						count += 1
+				
+				# Проверка правого крайнего элемента
+				elif (Col == colLength - 1):
+					if  (el < arr[Row - 1][Col])\
+					and (el < arr[Row + 1][Col])\
+					and (el < arr[Row][Col - 1])\
+					and (el < arr[Row + 1][Col - 1])\
+					and (el < arr[Row - 1][Col - 1]):
+						count += 1
+				
+				# Проверка любых не крайних элементов
+				else:
+					if  (el < arr[Row - 1][Col - 1])\
+					and (el < arr[Row - 1][Col])\
+					and (el < arr[Row - 1][Col + 1])\
+					and (el < arr[Row + 1][Col - 1])\
+					and (el < arr[Row + 1][Col])\
+					and (el < arr[Row + 1][Col + 1])\
+					and (el < arr[Row][Col - 1])\
+					and (el < arr[Row][Col + 1]):
+						count += 1
+
+	return count
+
+
+# Находит сумму элементов выше главной диагонали
+def sumAbs(arr):
+	sum = 0
+
+	startIndex = 1
+	for Row in range(rowLength - 1):
+		for Col in range(startIndex, colLength):
+			sum += abs(arr[Row][Col])
+
+		startIndex += 1
+	
+	return sum
+
+arr = makeRandomArr(rowLength, colLength, 0, 9)
+
+print('_______ Исходный массив _______')
+for Row in range(rowLength):
+	print(arr[Row])
+print('_______________________________')
+
+print('Количество локальных минимумов: {0}'.format(findLocalMin(arr)))
+print('Сумма модулей элементов выше главной диагонали: {0}'.format(sumAbs(arr)))
